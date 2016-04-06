26967019507bf9d16f152327f241380202883b7f
Nari <gb.966.komawari@gmail.com>
2016-04-06 10:14:50 -0700

    newer

--- app/src/main/java/com/android/test/testproject/MyActivity.java
+++ app/src/main/java/com/android/test/testproject/MyActivity.java
@@ -24,6 +24,8 @@ public class MyActivity extends AppCompatActivity {
       public void onClick(View view) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
             .setAction("Action", null).show();
+
+
       }
     });
   }
